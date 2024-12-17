5b1f595ca8ad60029b1fed74b9e394143ce50740
di5codan <di5codan@protonmail.com>
2024-12-17 02:25:30 -0600

    Dynamic routing test

--- benkyo/benkyo.py
+++ benkyo/benkyo.py
@@ -1,34 +1,50 @@
 import reflex as rx
 
 from rxconfig import config
+
+import re
 from .file_parse import *
 
 
 class State(rx.State):
-    """The app state."""
-
-    ...
+    @rx.var
+    def test(self) -> str:
+        args = self.router.page.params
+        chapter = args.get("genki_chapter_num", [])
+        return f"Chapter number: {chapter}"
 
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    chapters = file_parse_incl("jisho/Genki/","Chapter")
     return rx.container(
         rx.vstack(
             rx.text("This will be a cool frontend for my Jisho notes repository!"),
             rx.text("今ここで何もがない ¯\_(⊙_ʖ⊙)_/¯"),
+            rx.link("Genki Page",href="/genki"),
             rx.text("Shoutout to さおりさん + トム・ハンクス")
         ),
-        rx.text("Genki Chapters"),
+    )
+
+def genki_chapter_link(chapter: str):
+    #return rx.text(f"{chapter}")
+    return rx.link(f"Chapter {chapter}", href=f"/genki/{chapter}")
+
+@rx.page(route="/genki")
+def genki_overview():
+    #chapters = file_parse_incl("jisho/Genki/","Chapter")
+    chapters = list(range(0,24))
+    return rx.vstack(
+        rx.heading("Genki Chapters"),
         rx.foreach(chapters,genki_chapter_link)
     )
 
+@rx.page(route="/genki/[genki_chapter_num]")
+def genki_chapter_page():
+    #test = str(rx.State.genki_chapter_num)
+    #print(rx.State.genki_chapter_num)
+    #file = open(f"jisho/Genki/Chapter-{str(rx.State.genki_chapter_num)}.md","r")
+    #print("ah")
+    return rx.text(State.test)
 
-def genki_chapter_link(chapter: str):
-    return rx.text(chapter)
-
-
-#@rx.page(route="/genki_chapter/[num]")
-#def 
 app = rx.App()
 app.add_page(index)
